e2cfc319a2bb6ee8af8c39957b3d796edfca3b45
tanaxer01 <jpbrione@gmail.com>
2024-02-05 18:58:24 -0300

    set 3 ready

--- run.sh
+++ run.sh
@@ -6,22 +6,21 @@ run_set() {
     case $1 in
         1)
             A=1
-            B=9
             ;;
         2)
             A=9
-            B=17
             ;;
         3)
             A=17
-            B=25
+            ;;
+        4)
+            A=25
             ;;
         *)
             A=0
-            B=0
     esac
             
-    for ((i = $A; i < $B; i++)); do
+    for ((i = $A; i < $A + 8; i++)); do
         challenge="src/ch$(printf "%02d" "$i").py"
         if [ -e $challenge ]; then 
             python $challenge
--- src/ch24.py
+++ src/ch24.py
@@ -1,48 +1,49 @@
 from ch02 import xor
+from ch03 import english_score
 from ch21 import MT19937
 from time import time
 from math import ceil
 
-from ch22 import guess_timestamp_seed
+def PRNG_transform(text: bytes, seed: int) -> bytes:
+    new_len = len(text)
+    ks_len = ceil(new_len/4)
 
-def PRNG_transform(plain_text: bytes, seed: int) -> bytes:
-    pt_len = len(plain_text)
-    ks_len = ceil(pt_len/4)
-
-    prng = MT19937(seed & 0xFFFF)
-    ks_nums  = [ prng.extract_number() for _ in range(ks_len) ]
-    ks_bytes = b"".join( n.to_bytes(4, "big") for n in ks_nums )[:pt_len]
-
-    return xor(plain_text, ks_bytes)
-
-def detect_if_prng(key: int, seed: int) -> bool:
+    assert seed <= 0xFFFF
     prng = MT19937(seed)
-    nums  = [ prng.extract_number() for _ in range(MT19937.n) ]
+    ks_nums  = [ prng.extract_number() for _ in range(ks_len) ]
+    ks_bytes = b"".join( n.to_bytes(4, "big") for n in ks_nums )[:new_len]
 
-    return any( i == key for i in nums )
+    return xor(text, ks_bytes)
+
+def guess_16_bit_seed(cipher_text: bytes):
+    max_score = seed = 0
+    for i in range(0xFFFF):
+        posible_pt = PRNG_transform(cipher_text, i)
+        try:
+            score = english_score( posible_pt[2:].decode() )
+            if score > max_score:
+                max_score = score
+                seed = i
+
+                print(posible_pt.hex())
+        except:
+            pass
+
+    return seed
 
 if __name__ == "__main__":
     now = int(time())
     prng = MT19937(now)
-    pt_num = prng.extract_number() & 0xFFFF
-    pt = pt_num.to_bytes(2, "big") + b"A" * 14
 
+    num = prng.extract_number() & 0xFFFF
+    known_text = num.to_bytes(2, "big") + (b"A" * 14)
+
+    prng = MT19937(now)
     seed = prng.extract_number() & 0xFFFF
-    ct = PRNG_transform(pt, seed)
+    ct   = PRNG_transform(known_text, seed)
 
-    guess = guess_timestamp_seed(int.from_bytes(ct[:4], "big"), 0, range(1, 2**16) )
-    assert guess != -1
+    guess  = guess_16_bit_seed(ct)
+    assert guess == seed
 
-
-    '''
-    pt = b"A" * 14
-
-    key_prng = MT19937( int(time()) )
-    key = b"".join( key_prng.extract_number().to_bytes(4, "big") for _ in range(4) )
-
-    a = PRNG_transform(b"a" * 20, 1234)
-    print(a)
-    b = PRNG_decrypt(a, 1234)
-    print(b)
-    '''
+    print("challenge 24:\n\tseed ->",guess)
 
